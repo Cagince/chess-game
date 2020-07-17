53cd4813c2004539dc9c730bef62e2903dbff513
cagin <nebu.gomm@gmail.com>
2020-07-17 18:02:55 +0200

    added chess board placeholder html

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="board-wrapper">
+        <div className="board">
+        </div>
+      </div>
+      
+
+    </div>
+  );
+}
+
+export default App;
